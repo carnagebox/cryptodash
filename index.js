ede7293a3ffc3f43abfd51e912d11d4605472794
Ryan Maulding <ryan@carnagebox.com>
2020-01-01 22:43:07 -0800

    Removed extranious data sets

--- index.js
+++ index.js
@@ -2,31 +2,14 @@ const Gdax = require('gdax');
 const blessed = require('blessed');
 const request = require('request-promise');
 const config = require('config');
-const fs = require('fs');
 
 // Create a screen object.
 const screen = blessed.screen({
   smartCSR: true
 });
 
-screen.title = "CBX:Dashboard";
+screen.title = 'CBX:Dashboard';
 pos = 0;
-
-var Equity = blessed.text({
-  content: 'Equity',
-  bg: 'blue',
-  fg: 'cyan',
-  top: pos++,
-  width: '100%'
-});
-screen.append(Equity);
-
-var USDEquity = blessed.text({
-  tags: true,
-  top: pos++,
-  width: '100%'
-});
-screen.append(USDEquity);
 
 var Wallet = blessed.text({
   content: 'Wallet',
@@ -45,14 +28,6 @@ var USDBalance = blessed.text({
 });
 screen.append(USDBalance);
 
-var _BTCBalance = 0.0;
-var BTCBalance = blessed.text({
-  tags: true,
-  top: pos++,
-  width: '100%'
-});
-screen.append(BTCBalance);
-
 var _ETHBalance = 0.0;
 var ETHBalance = blessed.text({
   tags: true,
@@ -60,39 +35,6 @@ var ETHBalance = blessed.text({
   width: '100%'
 });
 screen.append(ETHBalance);
-
-var PerTrade = blessed.text({
-  content: 'Per Trade',
-  bg: 'blue',
-  fg: 'cyan',
-  top: pos++,
-  width: '100%'
-});
-screen.append(PerTrade);
-
-var _USDPerTrade = 0.0;
-var USDPerTrade = blessed.text({
-  tags: true,
-  top: pos++,
-  width: '100%'
-});
-screen.append(USDPerTrade);
-
-var _BTCPerTrade = 0.0;
-var BTCPerTrade = blessed.text({
-  tags: true,
-  top: pos++,
-  width: '100%'
-});
-screen.append(BTCPerTrade);
-
-var _ETHPerTrade = 0.0;
-var ETHPerTrade = blessed.text({
-  tags: true,
-  top: pos++,
-  width: '100%'
-});
-screen.append(ETHPerTrade);
 
 var Hashrate = blessed.text({
   content: 'Hashrate',
@@ -167,10 +109,6 @@ function refreshWallet() {
         _USDBalance = Number.parseFloat(d['balance']);
         var _USDPercent = Math.round((_USDBalance/_USDEquity)*100);
         USDBalance.setContent('{yellow-fg}USD{/yellow-fg} ' + _USDBalance.toFixed(2) + ' (' + _USDPercent + '%)');
-      } else if (d['currency'] == 'BTC') {
-        _BTCBalance = Number.parseFloat(d['balance']);
-        var _BTCPercent = Math.round((_BTCBalance/_BTCEquity)*100);
-        BTCBalance.setContent('{yellow-fg}BTC{/yellow-fg} ' + _BTCBalance.toFixed(8) + ' (' + _BTCPercent + '%)');
       } else if (d['currency'] == 'ETH') {
         _ETHBalance = Number.parseFloat(d['balance']);
         var _ETHPercent = Math.round((_ETHBalance/_ETHEquity)*100);
@@ -283,16 +221,14 @@ function updateDelta() {
 updateDelta();
 
 var _websocketInit = false;
-var _BTCUSDExchange = NaN;
 var _ETHUSDExchange = NaN;
 var _USDEquity = NaN;
-var _BTCEquity = NaN;
 var _ETHEquity = NaN;
 
 function connectWebsocket() {
   try {
     const websocket = new Gdax.WebsocketClient(
-      ['BTC-USD','ETH-USD'],
+      ['ETH-USD'],
       gdax.feedUri,
       {
         key: gdax.key,
@@ -303,26 +239,11 @@ function connectWebsocket() {
     );
     websocket.on('message', data => {
       if(data['type'] == 'ticker') {
-        if(data['product_id'] == 'BTC-USD') {
-          _BTCUSDExchange = Number.parseFloat(data['price']);
-        } else if(data['product_id'] == 'ETH-USD') {
+        if(data['product_id'] == 'ETH-USD') {
           _ETHUSDExchange = Number.parseFloat(data['price']);
         }
-        _USDEquity = _USDBalance + (_BTCBalance * _BTCUSDExchange) + (_ETHBalance * _ETHUSDExchange);
-        var _USDTrade = (0.02 * _USDEquity).toFixed(2);
-        _BTCEquity = _USDEquity / _BTCUSDExchange;
-        var _BTCTrade = (0.02 * _BTCEquity).toFixed(5);
+        _USDEquity = _USDBalance + (_ETHBalance * _ETHUSDExchange);
         _ETHEquity = _USDEquity / _ETHUSDExchange;
-        var _ETHTrade = (0.02 * _ETHEquity).toFixed(5);
-        USDEquity.setContent('{yellow-fg}USD{/yellow-fg} ' + _USDEquity.toFixed(2) + ' '
-          + '{' + _USDEquityGainLossColor + '}'
-          + _USDEquityGainLossIndicator
-          + _USDEquityGainLoss
-          + '%{/' + _USDEquityGainLossColor + '}');
-        USDPerTrade.setContent('{yellow-fg}USD{/yellow-fg} ' + _USDTrade);
-        BTCPerTrade.setContent('{yellow-fg}BTC{/yellow-fg} ' + _BTCTrade);
-        ETHPerTrade.setContent('{yellow-fg}ETH{/yellow-fg} ' + _ETHTrade);
-        screen.render();
       }
       if(!_websocketInit) {
         _websocketInit = true;
@@ -339,50 +260,3 @@ function connectWebsocket() {
   }
 }
 connectWebsocket();
-
-var _USDEquityGainLoss = NaN;
-var _USDEquityGainLossIndicator = ' ';
-var _USDEquityGainLossColor = 'green-fg';
-var _GainLossLength = '--'
-
-function refreshGainLoss() {
-  if(!_websocketInit) {
-    setTimeout(refreshGainLoss, 100);
-    return;
-  }
-
-  var gainLoss = null;
-  var data  = fs.readFileSync('gainloss.json', {flag: 'a+'});
-  if(data != '') {
-    gainLoss = JSON.parse(data);
-    gainLoss.push({equity: _USDEquity, price: _ETHUSDExchange});
-  } else {
-    gainLoss = [{equity: _USDEquity, price: _ETHUSDExchange}];
-  }
-
-  var first = _USDEquity;
-  var o = gainLoss.shift();
-  var last  = o.equity;
-  if(gainLoss.length < (24*60*10)) {
-    gainLoss.unshift(o);
-  }
-
-  gainLoss = JSON.stringify(gainLoss);
-  fs.writeFileSync('gainloss.json', gainLoss, {flag: 'w'});
-
-  var diff = last - first;
-  if(diff > 0) {
-    _USDEquityGainLoss = (100 - (first / last * 100)).toFixed(1);
-    _USDEquityGainLossIndicator = '▼';
-    _USDEquityGainLossColor = 'red-fg';
-  } else {
-    _USDEquityGainLoss = (100 - (last / first * 100)).toFixed(1);
-    _USDEquityGainLossIndicator = '▲';
-    _USDEquityGainLossColor = 'green-fg';
-  }
-
-  //TODO: Insert StopLoss sale for 5% less than yesterdays price.
-
-  setTimeout(refreshGainLoss, 10*1000);
-}
-refreshGainLoss();
